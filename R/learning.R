54422e0da4dfccae6e0d92043ac59037d8b88e7f
Simon Storgaard Jensen <simonsj95@hotmail.com>
2024-01-24 11:32:10 +0100

    Commit after 7.8 Exercise

--- R/learning.R
+++ R/learning.R
@@ -1,4 +1,53 @@
-
 # Loading packages --------------------------------------------------------
 
 library(tidyverse)
+library(NHANES)
+
+glimpse(NHANES)
+str(NHANES)
+
+# Select specific columns -------------------------------------------------
+
+select(NHANES, Age, Weight, BMI)
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"))
+select(NHANES, ends_with("Day"))
+select(NHANES, contains("Age"))
+
+# Renaming all columns ----------------------------------------------------
+
+NHANES_small <- rename_with(NHANES, snakecase::to_snake_case)
+
+# Renaming specific column ------------------------------------------------
+
+NHANES_small <- rename(NHANES_small, sex = gender)
+NHANES_small
+View(NHANES_small)
+
+
+
+# Chaining the functions with the pipe ------------------------------------
+
+colnames(NHANES_small)
+NHANES_small %>%
+  colnames()
+
+NHANES_phys <- NHANES_small %>%
+  select(phys_active) %>%
+  rename(physically_active = phys_active)
+
+NHANES_small %>%
+  select(bp_sys_ave, education)
+
+NHANES_small %>%
+  rename(
+    bp_sys = bp_sys_ave,
+    bp_dia = bp_dia_ave)
+
+NHANES_small %>%
+  select(bmi, contains("age"))
+
+NHANES_small %>%
+  select(starts_with("BP")) %>%
+  rename(bp_systolic = bp_sys_ave)
